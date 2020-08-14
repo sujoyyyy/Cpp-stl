64a7fec6c3b8cf93f191d2f0e366deb21aaeeb96
Sujoy Datta <sujoydatta.2009@gmail.com>
2020-08-15 01:32:07 +0530

    set added

--- set.cpp
+++ set.cpp
@@ -3,4 +3,32 @@
 using namespace std;
 int main()
 {     
+   set<int>s;
+   s.insert(10);
+   s.insert(-2);
+   s.insert(5);
+   s.insert(5);
+   s.insert(5);
+   for(int x:s)  //O(logN): best
+   cout<<x<<endl;
+   auto it= s.find(-2);
+   if(it==s.end())
+   {
+       cout<<"Not present"<<endl;
+   }
+   else
+   {
+       cout<<"Present "<<*it<<endl;
+   }
+   auto it2= s.lower_bound(-1);
+   auto it3= s.upper_bound(5);
+   cout<<*it3<<" "<<*it2<<endl;
+   
+   
+
+
+
+
+
+    return 0;
 }
